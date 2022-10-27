4e4b77b77e97844aefc9fe02f62835c9a5abcaac
GoGaGTR <gogagtr@gmail.com>
2022-10-27 22:02:18 +0300

    OK version *2

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -4,22 +4,49 @@ public class Radio {
 
     private int currentRadioStation;
     private int currentVolume;
+    private int numberStation = 10;
 
-    public void setCurrentRadioStation(int currentRadioStation) {
-        if (currentRadioStation < 0) {
+
+    public Radio() {
+    }
+
+    public Radio(int numberStation) {
+        this.numberStation = numberStation;
+    }
+
+
+
+    public int getNumberStation() {
+        return numberStation;
+    }
+
+    public void setNumberStation(int numberStation) {
+        if (numberStation < 0) {
             return;
         }
-        if (currentRadioStation > 9) {
+        this.numberStation = numberStation;
+    }
+
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation > numberStation - 1) {
+            return;
+        }
+        if (currentRadioStation < 0) {
             return;
         }
         this.currentRadioStation = currentRadioStation;
     }
 
 
-    public void nextRadioStation() {
 
-        if (currentRadioStation < 9) {
-            currentRadioStation = currentRadioStation + 1;
+    public void nextRadioStation() {
+        if (currentRadioStation < numberStation - 1) {
+            currentRadioStation++;
         } else {
             currentRadioStation = 0;
         }
@@ -28,40 +55,26 @@ public class Radio {
 
     public void prevRadioStation() {
         if (currentRadioStation > 0) {
-            currentRadioStation = currentRadioStation - 1;
+
+            currentRadioStation--;
         } else {
-            currentRadioStation = 9;
+            currentRadioStation = numberStation - 1;
         }
     }
 
-
-    public int getCurrentRadioStation() {
-        return currentRadioStation;
-    }
-
-
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
-            return;
-        }
-        if (currentVolume > 10) {
-            return;
-        }
-        this.currentVolume = currentVolume;
-    }
-
-
-    public void setToMaxVolume() {
-        currentVolume = 10;
-    }
-
-
-    public void setToMinVolume() {
-        currentVolume = 0;
-    }
 
 
     public int getCurrentVolume() {
         return currentVolume;
     }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > 100) {
+            return;
+        }
+        if (currentVolume < 0) {
+            return;
+        }
+        this.currentVolume = currentVolume; }
+
 }
